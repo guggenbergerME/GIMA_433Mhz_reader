b0262631badd1f7718cae284b61a6822ec5ca0a0
guggenbergerME <github@guggenberger.me>
2024-09-09 09:51:27 +0200

    Scan

--- src/main.cpp
+++ src/main.cpp
@@ -1,137 +1,62 @@
-#include <SPI.h>
-#include <Ethernet.h>
-#include <PubSubClient.h>
+/*
+  Simple example for receiving with Rssi output.
+  
+  https://github.com/sui77/rc-switch/
+  https://github.com/LSatan/SmartRC-CC1101-Driver-Lib
+  ----------------------------------------------------------
+  Mod by Little Satan. Have Fun!
+  ----------------------------------------------------------
+*/
+#include <ELECHOUSE_CC1101_SRC_DRV.h>
+#include <RCSwitch.h>
 
-//************************************************************************** LAN Network definieren N
-byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
-IPAddress ip(192, 168, 1, 22); //comment this line if you are using DHCP
-IPAddress server(192, 168, 1, 5); // mqtt Server
-EthernetClient ethClient;
+int pin; // int for Receive pin.
 
-//************************************************************************** mqtt Referenz
-PubSubClient client(ethClient);
+RCSwitch mySwitch = RCSwitch();
 
-
-//************************************************************************** Intervalle
-long lastReconnectAttempt = 0;
-const int reconnectInterval = 5000;
-
-unsigned long previousMillis_mqttCHECK = 0; // Windstärke prüfen
-unsigned long interval_mqttCHECK = 900; 
-
-//************************************************************************** Variablen
-long letzte_meldung = 0;
-char publish_meldung[50];
-int zaehler = 0;
-
-//************************************************************************** Pinout
-const int RELAIS_AUF = 2; // Relais-Pin am Arduino
-
-//************************************************************************** Funktionsprototypen
-void loop                       ();
-void setup                      ();
-void reconnect                  ();
-void callback(char* topic, byte* payload, unsigned int length);
-
-
-//************************************************************************** Zeitschleifen
-unsigned long previousMillis_mqtt = 0; // mqtt abrufen
-unsigned long interval_mqtt = 50; 
-
-//######################################################################## setup
 void setup() {
+  Serial.begin(9600);
 
-  // Serielle Schnittstelle
-  Serial.begin(115200);
+#ifdef ESP32
+pin = 4;  // for esp32! Receiver on GPIO pin 4. 
+#elif ESP8266
+pin = 4;  // for esp8266! Receiver on pin 4 = D2.
+#else
+pin = 0;  // for Arduino! Receiver on interrupt 0 => that is pin #2
+#endif    
 
-
-  // Ethernet starten
-  Ethernet.begin(mac, ip);
-  // Pause Netzwerk Antwort
-  delay(1500);
-
-  // Client starten und Callback abrufen
-  client.setServer(server, 1883);
-  client.setCallback(callback);
-
-  // Pins belegen
-  pinMode(RELAIS_AUF, OUTPUT);
-  // Ausgang nach Neustart setzen
-  digitalWrite(RELAIS_AUF, !LOW);
-
-}
-
-// ######################################################################## mqtt callback
-void callback(char* topic, byte* payload, unsigned int length) {
-
-
-    if (strcmp(topic,"GIMA_Technik/TEST_ARDUINO_LAN/Steuerung")==0) {
-
-        // Kanal A
-        if ((char)payload[0] == 'o' && (char)payload[1] == 'n') {  
-                 Serial.println("relais_A -> AN");
-                  digitalWrite(RELAIS_AUF, !HIGH);
-                 delay(10);
-              }
-
-        if ((char)payload[0] == 'o' && (char)payload[1] == 'f' && (char)payload[2] == 'f') {  
-                 Serial.println("relais_A -> AUS");
-                  digitalWrite(RELAIS_AUF, !LOW);
-                 delay(10);
-              }
-      } 
-}
-
-
-
-// ######################################################################## mqtt reconnect
-void reconnect() {
-  // Loop until we're reconnected
-   while (!client.connected()) {
-    /*
-    Serial.print("Verbindung zu mqtt IP: ");
-    Serial.print(server);
-    Serial.println("");
-    */
-    // Create a random client ID
-    String clientId = "GIMA-Technik";
-    clientId += String(random(0xffff), HEX);
-    // Attempt to connect
-    if (client.connect("GIMA_TEST2", "daten1", "qwerqwer")) {
-      Serial.println("mqtt lesen ....");
-      // ... and resubscribe
-      client.subscribe("GIMA_Technik/TEST_ARDUINO_LAN/Steuerung");
-        
-    } else {
-      Serial.print("failed, rc=");
-      Serial.print(client.state());
-      Serial.println(" try again in 5 seconds");
-      // Wait 5 seconds before retrying
-      delay(5000);
-    }
+  if (ELECHOUSE_cc1101.getCC1101()){       // Check the CC1101 Spi connection.
+  Serial.println("Connection OK");
+  }else{
+  Serial.println("Connection Error");
   }
-}
 
-//######################################################################## loop
+//CC1101 Settings:                (Settings with "//" are optional!)
+  ELECHOUSE_cc1101.Init();            // must be set to initialize the cc1101!
+//ELECHOUSE_cc1101.setRxBW(812.50);  // Set the Receive Bandwidth in kHz. Value from 58.03 to 812.50. Default is 812.50 kHz.
+//ELECHOUSE_cc1101.setPA(10);       // set TxPower. The following settings are possible depending on the frequency band.  (-30  -20  -15  -10  -6    0    5    7    10   11   12)   Default is max!
+  ELECHOUSE_cc1101.setMHZ(433.92); // Here you can set your basic frequency. The lib calculates the frequency automatically (default = 433.92).The cc1101 can: 300-348 MHZ, 387-464MHZ and 779-928MHZ. Read More info from datasheet.
+
+  mySwitch.enableReceive(pin);  // Receiver on
+
+  ELECHOUSE_cc1101.SetRx();  // set Receive on
+  
+}
 void loop() {
+ 
+  if (mySwitch.available()){
 
-  // mqtt Daten senden     
-  if (!client.connected()) {
-      reconnect();
-    }
-    client.loop(); 
-
-
-  // Nach einem Zeitintervall eine Nachricht publishen
-  long now = millis();
-  if (now - letzte_meldung > 6000) {
-    letzte_meldung = now;
-    ++zaehler;
-    snprintf (publish_meldung, 75, "UNO Zähler #%ld", zaehler);
-    Serial.print("## >> mqtt - Nachricht publish: ");
-    Serial.println(publish_meldung);
-    client.publish("GIMA_Technik/TEST_ARDUINO_LAN/Zaehler", publish_meldung);
+    Serial.print("Received ");
+    Serial.print( mySwitch.getReceivedValue() );
+    Serial.print(" / ");
+    Serial.print( mySwitch.getReceivedBitlength() );
+    Serial.print("bit ");
+    Serial.print("Protocol: ");
+    Serial.println( mySwitch.getReceivedProtocol() );
+     
+    Serial.print("RSSI: ");
+    Serial.println(ELECHOUSE_cc1101.getRssi());
+    
+    mySwitch.resetAvailable();
   }
-
 }
-
